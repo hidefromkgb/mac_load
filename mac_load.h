7e85b68359536e818a7425f107142451991bdc02
hidefromkgb <hidefromkgb@gmail.com>
2021-12-07 22:34:01 +0100

    Add control tints

--- mac_load.h
+++ mac_load.h
@@ -335,6 +335,12 @@ enum {
     NSOpenGLCPHasDrawable            = 314,
     NSOpenGLCPMPSwapsInFlight        = 315,
 };
+enum {
+    NSDefaultControlTint = 0,
+    NSBlueControlTint = 1,
+    NSGraphiteControlTint = 6,
+    NSClearControlTint = 7,
+};
 
 
 
